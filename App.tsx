041fb5363fc45068ab0b56e3cf3d5c6ca752113b
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-11-22 11:19:13 -0300

    feat: App fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let [fontsLoaded] = Font.useFonts({
+		'digital-number': require('./assets/fonts/Digital_Play_St.ttf'),
+		'white-dream': require('./assets/fonts/WhiteDream.otf'),
+	})
+
+	if (!fontsLoaded) {
+		return <AppLoading />
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
