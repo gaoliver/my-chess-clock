53d4bedf874643a7ea726f1dbc9fa143730d6ff6
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-11-22 21:21:55 -0300

    feat: Switch component + landscape mode

---
+++ src/components/AppSwitcher.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StyleSheet, Text, View, Switch } from 'react-native'
+import Colors from '../constants/Colors'
+
+interface IProps {
+	label?: string
+	value?: boolean
+	onValueChange?: () => void
+}
+
+const translator = (props: IProps) => ({
+	label: props.label ? props.label : '',
+	value: props.value ? props.value : false,
+	onValueChange: props.onValueChange ? props.onValueChange : () => {},
+})
+
+export const AppSwitcher = (props: IProps) => {
+	const { label, value, onValueChange } = translator(props)
+
+	const styles = StyleSheet.create({
+		line: {
+			flexDirection: 'row',
+			alignItems: 'center',
+		},
+		label: {
+			marginRight: 20,
+			fontSize: 20,
+			color: Colors.screenTextColor,
+		},
+	})
+
+	return (
+		<View style={styles.line}>
+			<Text style={styles.label}>{label}</Text>
+			<Switch
+				trackColor={{ false: '#767577', true: Colors.themeColor }}
+				onValueChange={onValueChange}
+				value={value}
+			/>
+		</View>
+	)
+}
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { AppHeader } from '../components'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+
+import * as gameActions from '../redux/actions'
+import { AppHeader, AppSwitcher } from '../components'
+import { ApplicationState } from '../redux'
 
 const SettingsScreen = () => {
+	const dispatch = useDispatch()
+	const settings = useSelector((state: ApplicationState) => state.settings)
+	const [translator, setTranslator] = useState(settings)
+
+	const toggleLandscape = () => {
+		setTranslator({
+			...translator,
+			landscape: !translator.landscape ? true : false,
+		})
+	}
+
+	useEffect(() => {
+		dispatch(gameActions.setSettings(translator))
+	}, [translator])
+
 	return (
 		<View>
 			<AppHeader title="Settings" hasGoBack />
-			<Text>Settings Screen</Text>
+			<AppSwitcher
+				label="Landscape"
+				value={translator.landscape}
+				onValueChange={toggleLandscape}
+			/>
 		</View>
 	)
 }
